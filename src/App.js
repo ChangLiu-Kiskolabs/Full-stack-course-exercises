b3186a47b1750a6c5324e39a994ab4736a0eac36
Chang Liu <53565704+ChangLiu-Kiskolabs@users.noreply.github.com>
2021-04-08 12:45:29 +0300

    submission

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+const course = "Half Stack application development";
+const part1 = "Fundamentals of React";
+const exercises1 = 10;
+const part2 = "Using props to pass data";
+const exercises2 = 7;
+const part3 = "State of a component";
+const exercises3 = 14;
+const totalnumber = exercises1 + exercises2 + exercises3;
+
+const Header = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{course}</h1>
     </div>
   );
-}
+};
+
+const Part1 = () => {
+  return (
+    <div>
+      <p>
+        {part1} {exercises1}
+      </p>
+    </div>
+  );
+};
+const Part2 = () => {
+  return (
+    <div>
+      <p>
+        {part2} {exercises2}
+      </p>
+    </div>
+  );
+};
+const Part3 = () => {
+  return (
+    <div>
+      <p>
+        {part3} {exercises3}
+      </p>
+    </div>
+  );
+};
+
+const Content = () => {
+  return (
+    <div>
+      <Part1 />
+      <Part2 />
+      <Part3 />
+    </div>
+  );
+};
+
+const Total = () => {
+  return (
+    <div>
+      <p>Number of exercises {totalnumber}</p>
+    </div>
+  );
+};
+
+const App = () => {
+  return (
+    <div>
+      <Header course={course} />
+      <Content />
+      <Total totalnumber={totalnumber} />
+    </div>
+  );
+};
 
 export default App;
